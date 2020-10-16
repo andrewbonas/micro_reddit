a5c7aa60f8707fc6eafc57a16d166a8ba134846e
Andrew Bonas <aabonas@hotmail.com>
2020-10-16 12:55:17 -0700

    comment model added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { maximum: 500, too_long: '%<count> characters is the maximum allowed', minimum: 10 }, allow_blank: false
 end
